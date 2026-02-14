51cdda27a84c59731ba7e5166ff9aa2b53cd979a
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-15 06:29:37 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ScreenRecorder from './screen-recorder'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ScreenRecorder />
+  </React.StrictMode>
+)

80fbdeedd6817a27a134bb0d032692c8dd547fdf
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-15 09:55:45 +0800

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ScreenRecorder from './screen-recorder'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ScreenRecorder />
-  </React.StrictMode>
-)

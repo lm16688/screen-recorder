212e1b530a41373d8b22b53257f3343d28d50c3e
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-15 09:56:33 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScreenRecorder from './screen-recorder';
+
+function App() {
+  return (
+    <div className="App">
+      <ScreenRecorder />
+    </div>
+  );
+}
+
+export default App;

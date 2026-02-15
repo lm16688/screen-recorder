7be0247933a757eb5a02da74907b2506d3923f7c
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-15 12:23:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/[你的仓库名]/', // 如果是用户页面，格式为 /[仓库名]/
+  base: '/screen-recorder/', // 如果是用户页面，格式为 /[仓库名]/
   server: {
     port: 3000
   }
